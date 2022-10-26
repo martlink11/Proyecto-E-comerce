8bb188a7ae7b4a5e3d6c2d796d03d1fd2fddc998
Martin Alvarez <m.alvarez110401@gmail.com>
2022-10-26 11:45:14 -0300

    formulario de compra, arreglos y orden del codigo en general

--- js/cart.js
+++ js/cart.js
@@ -1,102 +1,210 @@
-const urlCarrito = CART_INFO_URL + USER_ID + EXT_TYPE ;
-const templateCarrito = document.getElementById("template-carrito").content ;
-const fragment = document.createDocumentFragment() ;
-const contenido = document.getElementById("cart-container"); 
+// constantes para fetch y template
+const urlCarrito = CART_INFO_URL + USER_ID + EXT_TYPE;
+const templateCarrito = document.getElementById("template-carrito").content;
+const fragment = document.createDocumentFragment();
+const contenido = document.getElementById("cart-container");
+const templateCostos = document.getElementById("template-costos").content;
+const fragmentoCostos = document.createDocumentFragment();
+const contenidoCostos = document.getElementById("Costos-totales");
 
+// constantes para acceder a los objetos del formulario
+const NumTarjeta = document.getElementById("Num-tarjeta");
+const CodiogoSeg = document.getElementById("Cod-Seg");
+const Fecha = document.getElementById("FechaVencimiento");
+const NumCuenta = document.getElementById("NumCuenta")
+const Calle = document.getElementById("Calle");
+const NumCalle = document.getElementById("Num-calle");
+const Esquina = document.getElementById("Esquina");
+const btnCompra = document.getElementById("finalizar-compra");
+const TarjetaCredito = document.getElementById("TarjetaCredito");
+const TransfCuenta = document.getElementById("CuentaBancaria");
+const envioPremium = document.getElementById("envio-premium");
+const envioExpress = document.getElementById("envio-express");
+const envioStandar = document.getElementById("envio-standar");
 
-
-
-
-/* Otra posible opcion para hacer la peticion a la URL
-    document.addEventListener("DOMContentLoaded", function(e){
-
-    getJSONData(urlCarrito).then(function(resultObj){
-        if (resultObj.status === "ok"){
-            Carritus = resultObj.data.articles;
-            mostrarArticulo(Carritus);
-}
-})
-    
-}); */
-
-document.addEventListener("DOMContentLoaded", async () => {   
+// fetch
+document.addEventListener("DOMContentLoaded", async () => {
     const fetchCarrito = await getJSONData(urlCarrito)
     if (fetchCarrito.status === "ok") {
         InfoCarrito = fetchCarrito.data.articles;
-      //  mostrarArticulo(InfoCarrito);
-        objetos(InfoCarrito);   
+        objetos(InfoCarrito);
+        costos(InfoCarrito);
     }
-    
-}); 
 
-/* Imprimir los articulos utilizando innerHTML
+});
 
- function mostrarArticulo(array){
-    let cart = "";
-    for( let i = 0 ; i < array.length; i++){
-let articulo = array[i];
-cart += `
-<div class="row align-items-start">
-    <div class="col-2">
-        <img src="${articulo.image}" style="width: 150px;">
-    </div>
-    <div class="col-2">
-        <p>${articulo.name}</p>
-    </div>
-    <div class="col-2">
-    <label>${articulo.currency} <p id="precio">${articulo.unitCost}</p></label>
-    </div>
-    <div class="col-2">
-        <button class="btn btn-danger" id="restar">-</button><input type="number" value="${articulo.count}" min="1" id="cantidad" style="width:30px" oninput="${precioTotal()}"> <button id="sumar" class="btn btn-info">+</button>
-    </div>
-    <div class="col-2">
-        <b id="subtotal" >${articulo.currency}</b>
-    </div>   
-</div>
-
-`    
-}
-
-    document.getElementById("cart-container").innerHTML += cart;
-    
-};
-*/
-
-// imprimiendo los articulos con template, fragment y appendChild
+// mostrando los articulos con template, fragment y appendChild
+let contador = 1
 const objetos = InfoCarrito => {
     InfoCarrito.forEach(element => {
         templateCarrito.querySelector(".name").textContent = element.name
         templateCarrito.querySelector('#precio').textContent = element.currency
-        templateCarrito.querySelector('#precio').textContent += " " +element.unitCost
+        templateCarrito.querySelector('#precio').textContent += " " + element.unitCost
         templateCarrito.querySelector('img').setAttribute("src", element.image)
         templateCarrito.querySelector('#cantidad').value = element.count
         templateCarrito.querySelector('#subtotal').textContent = element.currency
-        templateCarrito.querySelector('#subtotal').textContent += " " +element.unitCost 
+        templateCarrito.querySelector('#subtotal').textContent += " " + element.unitCost
+
+
 
         const clone = templateCarrito.cloneNode(true);
         fragment.appendChild(clone)
-        
-        let contador = 1
-contenido.addEventListener('click', (e) =>{
-    if(e.target.classList.contains('btn-success')){
-        contador ++
-        document.getElementById("cantidad").value = contador;
-        document.getElementById("subtotal").innerText = element.currency + " " + parseInt(contador) * element.unitCost;
-        
-        
-    }
-    if(e.target.classList.contains('btn-danger')){
-        contador --
-        document.getElementById("cantidad").value = contador;
-        document.getElementById("subtotal").innerText = element.currency + " " +parseInt(contador) * element.unitCost;
-    }
-})
 
-});
+
+        // evento click para modificar precios al sumar o restar
+        contenido.addEventListener('click', (e) => {
+            if (e.target.classList.contains('btn-success')) {
+                contador++
+                document.getElementById("cantidad").value = contador;
+                document.getElementById("subtotal").innerText = element.currency + " " + parseInt(contador) * element.unitCost;
+                document.getElementById('costo-subtotal').innerText = element.currency + " " + parseInt(contador) * element.unitCost
+                document.getElementById('suma-total').innerText = element.currency + " " + calculoTotal(element.unitCost, contador)
+                document.getElementById('costo-envio').innerText = element.currency + " " + calculoEnvio(element.unitCost, contador)
+
+
+
+
+            }
+            if (e.target.classList.contains('btn-danger')) {
+                contador--
+                document.getElementById("cantidad").value = contador;
+                document.getElementById("subtotal").innerText = element.currency + " " + parseInt(contador) * element.unitCost;
+                document.getElementById('costo-subtotal').innerText = element.currency + " " + parseInt(contador) * element.unitCost
+                document.getElementById('suma-total').innerText = element.currency + " " + calculoTotal(element.unitCost, contador)
+                document.getElementById('costo-envio').innerText = element.currency + " " + calculoEnvio(element.unitCost, contador)
+            }
+        })
+
+
+        envioPremium.addEventListener('click', () => {
+            valorEnvio = 0.15;
+            document.getElementById('costo-envio').innerText = element.currency + " " + calculoEnvio(element.unitCost, contador)
+            document.getElementById('suma-total').innerText = element.currency + " " + calculoTotal(element.unitCost, contador)
+
+        })
+
+        envioExpress.addEventListener('click', () => {
+            valorEnvio = 0.07;
+            document.getElementById('costo-envio').innerText = element.currency + " " + calculoEnvio(element.unitCost, contador)
+            document.getElementById('suma-total').innerText = element.currency + " " + calculoTotal(element.unitCost, contador)
+        })
+
+        envioStandar.addEventListener('click', () => {
+            valorEnvio = 0.05;
+            document.getElementById('costo-envio').innerText = element.currency + " " + calculoEnvio(element.unitCost, contador)
+            document.getElementById('suma-total').innerText = element.currency + " " + calculoTotal(element.unitCost, contador)
+        })
+
+    });
     contenido.appendChild(fragment)
 }
 
+// imprimiendo el template del formulario de costos
+const costos = InfoCarrito => {
+    InfoCarrito.forEach(element => {
+        templateCostos.querySelector('#costo-subtotal').innerText = element.currency + " " + element.unitCost
+        templateCostos.querySelector('#costo-envio').innerText = element.currency + " " + element.unitCost
+        templateCostos.querySelector('#suma-total').innerText = element.currency + " " + element.unitCost
 
 
 
-    
+        const clonar = templateCostos.cloneNode(true);
+        fragmentoCostos.appendChild(clonar)
+    })
+    contenidoCostos.appendChild(fragmentoCostos)
+}
+
+
+// funcion para validar el formulario
+function validar(input, tipo = "error") {
+    let inp = input;
+
+    if (tipo === "error") {
+        inp.classList.remove("is-valid");
+        inp.classList.add("is-invalid");
+    } else {
+        inp.classList.remove("is-invalid");
+        inp.classList.add("is-valid");
+    }
+}
+
+
+// Evento de validacion
+btnCompra.addEventListener('click', (e) => {
+    e.preventDefault();
+    if (Calle.value.trim() != "") {
+        validar(Calle, "c")
+
+    } else {
+        validar(Calle)
+    }
+
+    if (NumCalle.value.trim() != "") {
+        validar(NumCalle, "c")
+
+    } else {
+        validar(NumCalle)
+    }
+
+    if (Esquina.value.trim() != "") {
+        validar(Esquina, "c")
+
+    } else {
+        validar(Esquina)
+    }
+
+    if (NumTarjeta.value.trim() != "") {
+        validar(NumTarjeta, "c")
+
+    } else {
+        validar(NumTarjeta)
+    }
+
+    if (NumCuenta.value.trim() != "") {
+        validar(NumCuenta, "c")
+
+    } else {
+        validar(NumCuenta)
+    }
+
+    if (Fecha.value.trim() != "") {
+        validar(Fecha, "c")
+
+    } else {
+        validar(Fecha)
+    }
+
+})
+
+// funcion para calcular el valor total del envio
+let valorEnvio = 0;
+function calculoEnvio(Objeto, cantidad) {
+    return (Objeto * cantidad) * valorEnvio
+}
+
+// function para hacer el calculo total 
+function calculoTotal(objeto, cantidad) {
+    return ((objeto * cantidad) * valorEnvio) + (objeto * cantidad)
+}
+
+// Eventos click para el tipo de de pago
+const txtPago = document.getElementById("pago")
+TarjetaCredito.addEventListener('click', () => {
+    NumCuenta.disabled = true
+    NumTarjeta.disabled = false
+    CodiogoSeg.disabled = false
+    Fecha.disabled = false
+    txtPago.textContent = "Pago con Tarjeta"
+})
+
+
+TransfCuenta.addEventListener('click', () => {
+    NumCuenta.disabled = false
+    NumTarjeta.disabled = true
+    CodiogoSeg.disabled = true
+    Fecha.disabled = true
+    txtPago.textContent = "Pago con transferencia bancaria"
+})
+
+
+
--- js/product-info.js
+++ js/product-info.js
@@ -17,13 +17,13 @@ document.addEventListener("DOMContentLoaded", async () => {
     if (linkearComentarios.status === "ok") {
         comentarios = linkearComentarios.data;
         mostarComentarios();
-        
+
     }
 
 });
 
 
-
+// funcion para mostrar info del producto consumido de la api
 
 function mostrarInfo() {
     let contenidoHTML = ``;
@@ -54,9 +54,9 @@ function mostrarInfo() {
     document.getElementById("informacion").innerHTML = contenidoHTML;
     let imagenesDelProducto = `   
     <div id="carouselExampleIndicators" class="carousel carousel-dark slide w-75" data-bs-ride="carousel">
-  <div class="carousel-indicators">
-    ${ botonesCarrusel(infoProductos.images)}
-  </div>
+        <div class="carousel-indicators">
+        ${botonesCarrusel(infoProductos.images)}
+        </div>
   <div class="carousel-inner">
     ${imagenesCarrusel(infoProductos.images)}
   </div>
@@ -70,42 +70,44 @@ function mostrarInfo() {
   </button>
 </div>
 `
-document.getElementById("carrusel").innerHTML = imagenesDelProducto ;
-agregarAlCarrito.setAttribute("id",infoProductos.id)
+    document.getElementById("carrusel").innerHTML = imagenesDelProducto;
+    agregarAlCarrito.setAttribute("id", infoProductos.id)
 }
 
+// funciones para el carrusel
 
-
-function botonesCarrusel(array){
-let botoncito = "" ;
-    for( let b = 0 ; b < array.length; b++){
-    if (b === 0){
-botoncito += `<button type="button" data-bs-target="#carouselExampleDark" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>`
-    } else { 
-        botoncito += `<button type="button" data-bs-target="#carouselExampleDark" data-bs-slide-to="${b}" aria-label="Slide ${b + 1}"></button>`
+function botonesCarrusel(array) {
+    let botoncito = "";
+    for (let b = 0; b < array.length; b++) {
+        if (b === 0) {
+            botoncito += `<button type="button" data-bs-target="#carouselExampleDark" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>`
+        } else {
+            botoncito += `<button type="button" data-bs-target="#carouselExampleDark" data-bs-slide-to="${b}" aria-label="Slide ${b + 1}"></button>`
+        }
     }
+    return botoncito
 }
-return botoncito
-}
-function imagenesCarrusel(array){
+function imagenesCarrusel(array) {
     let imgCarrusel = "";
-    for (let i = 0 ; i < array.length; i++){
+    for (let i = 0; i < array.length; i++) {
         let imagencita = array[i];
-        if (i === 0){
-            imgCarrusel += ` <div class="carousel-item active">
-            <img src="${imagencita}" class="d-block w-100" alt="Error">
-          </div> `
+        if (i === 0) {
+            imgCarrusel += ` 
+            <div class="carousel-item active">
+                <img src="${imagencita}" class="d-block w-100" alt="Error">
+            </div> `
         } else {
             imgCarrusel += `
             <div class="carousel-item">
-      <img src="${imagencita}" class="d-block w-100" alt="Error">
-    </div>
+                <img src="${imagencita}" class="d-block w-100" alt="Error">
+            </div>
             `
         }
     }
-   return imgCarrusel
+    return imgCarrusel
 }
 
+// Funcion para mostrar los comentarios que se encuentra en la api
 
 function mostarComentarios() {
     let contenido = "";
@@ -157,12 +159,13 @@ function mostarComentarios() {
 </div>
         
         `
-        
+
     }
     document.getElementById("comentarios").innerHTML += contenido;
 
 }
 
+// funcion que agrega un nuevo comentario
 
 function agregarComentario() {
     let lista = document.getElementById("comentarios")
@@ -228,7 +231,7 @@ function agregarComentario() {
 
 }
 
-
+// evento click para agregar un comentario
 
 boton.addEventListener("click", () => {
     if (comment.value != "" && puntuacion.value != 0) {
@@ -243,23 +246,25 @@ boton.addEventListener("click", () => {
     }
 });
 
-function productosRelacionados(){
+// funcion para los productos relacionados
+
+function productosRelacionados() {
     let relatedProduct = "";
     for (let r = 0; r < infoProductos.relatedProducts.length; r++) {
         let otherProduct = infoProductos.relatedProducts[r];
-    relatedProduct += `
+        relatedProduct += `
     <div class="col-md-4">
             <div class="card mb-4 shadow-sm custom-card cursor-active">
-              <img class="bd-placeholder-img card-img-top" src="`+ otherProduct.image +`"
+                <img class="bd-placeholder-img card-img-top" src="`+ otherProduct.image + `"
                 alt="Imgagen representativa">
-              <h3 class="m-3">`+ otherProduct.name +`</h3>
-              <div class="card-body">
-                <p class="card-text">Por mas informacion del producto
-                    <a  onclick="localStorage.setItem('productID', ` + otherProduct.id +` )" href="product-info.html">Clickea Aqui</a>
-                </p>
-              </div>
+                <h3 class="m-3">`+ otherProduct.name + `</h3>
+                <div class="card-body">
+                    <p class="card-text">Por mas informacion del producto
+                    <a  onclick="localStorage.setItem('productID', ` + otherProduct.id + ` )" href="product-info.html">Clickea Aqui</a>
+                    </p>
+                </div>
             </div>
-          </div>
+    </div>
     `
     }
     document.getElementById("otrosProductos").innerHTML += relatedProduct;
